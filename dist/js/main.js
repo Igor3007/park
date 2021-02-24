7ab06d560b4661ea7998662b48baeedb32698103
Igor3007 <50610898+Igor3007@users.noreply.github.com>
2021-02-24 17:45:05 +0300

    стили

--- dist/js/main.js
+++ dist/js/main.js
@@ -2244,6 +2244,53 @@ var swiper2 = new swiper__WEBPACK_IMPORTED_MODULE_3__["default"]('.infr-slider-m
     prevEl: '.infr-slider-prev'
   }
 });
+ymaps.ready(function () {
+  var myMap = new ymaps.Map('map-container', {
+    center: [55.751574, 37.573856],
+    zoom: 9,
+    controls: []
+  }, {
+    suppressMapOpenBlock: true
+  }),
+      // Создаём макет содержимого.
+  MyIconContentLayout = ymaps.templateLayoutFactory.createClass('<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'),
+      myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+    hintContent: 'Собственный значок метки',
+    balloonContent: 'Это красивая метка'
+  }, {
+    // Опции.
+    // Необходимо указать данный тип макета.
+    iconLayout: 'default#image',
+    // Своё изображение иконки метки.
+    iconImageHref: '/img/svg/ic_pen.svg',
+    // Размеры метки.
+    iconImageSize: [60, 60],
+    // Смещение левого верхнего угла иконки относительно
+    // её "ножки" (точки привязки).
+    iconImageOffset: [-30, -30]
+  }),
+      myPlacemarkWithContent = new ymaps.Placemark([55.661574, 37.573856], {
+    hintContent: 'Собственный значок метки с контентом',
+    balloonContent: 'А эта — новогодняя',
+    iconContent: '12'
+  }, {
+    // Опции.
+    // Необходимо указать данный тип макета.
+    iconLayout: 'default#imageWithContent',
+    // Своё изображение иконки метки.
+    iconImageHref: 'images/ball.png',
+    // Размеры метки.
+    iconImageSize: [48, 48],
+    // Смещение левого верхнего угла иконки относительно
+    // её "ножки" (точки привязки).
+    iconImageOffset: [-24, -24],
+    // Смещение слоя с содержимым относительно слоя с картинкой.
+    iconContentOffset: [15, 15],
+    // Макет содержимого.
+    iconContentLayout: MyIconContentLayout
+  });
+  myMap.geoObjects.add(myPlacemark).add(myPlacemarkWithContent);
+});
 
 /***/ })
 
